f96a042abc034a8104f7c8db6608bba2716559e2
Marc Mazer <cramrezam@gmail.com>
2015-03-28 17:14:52 -0400

    Remove browser bundle
    - preparing for move to browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,7 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var jscs = require('gulp-jscs');
-var rename = require('gulp-rename');
 var rm = require('gulp-rm');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var pkg = require('./package');
-var tasks = require('quincy/build/tasks');
 
 gulp.task('lint', function() {
   return gulp.src(['lib/**/*.js'])
@@ -22,27 +17,9 @@ gulp.task('test', function() {
     .pipe(mocha());
 });
 
-gulp.task('module', tasks.configure(tasks.module, {
-  prefix: 'quincy/',
-  module: pkg.name
-}));
-
-gulp.task('bundle', ['module'], function() {
-  return gulp.src(['target/*.js'])
-    .pipe(concat(pkg.name + '.js'))
-    .pipe(gulp.dest('bundle'));
-});
-
-gulp.task('build', ['bundle'], function() {
-  return gulp.src(['bundle/*.js', '!bundle/*.min.js'])
-  .pipe(uglify())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('bundle'));
-});
-
 gulp.task('clean', function() {
-  return gulp.src(['*.tgz','target/**', 'bundle/*.js'], { read: false })
+  return gulp.src(['*.tgz'], { read: false })
       .pipe( rm());
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['test']);
